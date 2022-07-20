b250262043a2c942595f22b3776f4f88f7ac91a2
Nicolas <nicolasfuentesg06@gmail.com>
2022-07-20 16:23:16 -0300

    Realizamos la subida de archivo por medio de firebase

--- app/src/main/java/com/example/vlogdeperros/AddFragment.kt
+++ app/src/main/java/com/example/vlogdeperros/AddFragment.kt
@@ -9,8 +9,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.vlogdeperros.databinding.FragmentAddBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 
 class AddFragment : Fragment() {
@@ -19,6 +24,12 @@ class AddFragment : Fragment() {
     private val binding get() = _binding!!
 
     private var mPhotoSelectedUri: Uri? = null
+
+    //Creamos las configuraciones iciales para la base de datos
+    private lateinit var mStorageReference: StorageReference
+    private lateinit var mDatabaseReference: DatabaseReference
+
+    private val PATH_DOG = "dogs"
 
     private val galleryLauncher =   registerForActivityResult(ActivityResultContracts.StartActivityForResult()){activityResult ->
 
@@ -54,14 +65,56 @@ class AddFragment : Fragment() {
                 openGallery()
             }
 
+            btnPublic.setOnClickListener {
+                uploadPhoto()
+            }
 
+        }
 
+        mStorageReference = FirebaseStorage.getInstance().reference
+        mDatabaseReference = FirebaseDatabase.getInstance().reference.child(PATH_DOG)
+    }
+
+    private fun uploadPhoto() {
+        binding.progressBar.visibility = View.VISIBLE
+        //Guardamos la foto en la base de datos de firebase
+        val storeReference = mStorageReference.child("my_photo")
+        val key = mDatabaseReference.push().key!!
+
+        if (mPhotoSelectedUri != null){
+            storeReference.putFile(mPhotoSelectedUri!!).addOnProgressListener {
+                val progress = (100 * it.bytesTransferred / it.totalByteCount).toDouble()
+                binding.progressBar.progress = progress.toInt()
+                binding.tvTitle.text = "$progress"
+            }
+                .addOnCompleteListener {
+                    binding.progressBar.visibility = View.GONE
+                 }
+
+                .addOnSuccessListener {
+                    Toast.makeText(context,getString(R.string.completed_upload), Toast.LENGTH_SHORT).show()
+                    it.storage.downloadUrl.addOnSuccessListener {
+                    savePhoto(key,it.toString(),binding.etTitle.text.toString().trim())
+                    binding.tvTitle.text = getString(R.string.title_public)
+                }
+
+            }
+                .addOnFailureListener{
+                Toast.makeText(context,getString(R.string.fail_upload), Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
     private fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         galleryLauncher.launch(intent)
+
+    }
+
+    private fun savePhoto(key:String, url:String, title:String){
+        val dog = DogDataClass(title = title, photoUrl = url)
+        mDatabaseReference.child(key).setValue(dog)
+
 
     }
 
