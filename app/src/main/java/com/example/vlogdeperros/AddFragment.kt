3bd2134ab83405b74e7f7e572c68d1efc9f513be
Nicolas <nicolasfuentesg06@gmail.com>
2022-07-20 16:06:50 -0300

    Creamos el intent para poder ingresar a nuestra galeria y tomar la foto de nuestras mascota

--- app/src/main/java/com/example/vlogdeperros/AddFragment.kt
+++ app/src/main/java/com/example/vlogdeperros/AddFragment.kt
@@ -1,10 +1,15 @@
 package com.example.vlogdeperros
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.vlogdeperros.databinding.FragmentAddBinding
 
 
@@ -13,14 +18,51 @@ class AddFragment : Fragment() {
     private var _binding: FragmentAddBinding? = null
     private val binding get() = _binding!!
 
+    private var mPhotoSelectedUri: Uri? = null
+
+    private val galleryLauncher =   registerForActivityResult(ActivityResultContracts.StartActivityForResult()){activityResult ->
+
+        if (activityResult.resultCode == RESULT_OK){
+
+            mPhotoSelectedUri = activityResult.data?.data
+            binding.apply {
+                tiTitle.visibility = View.VISIBLE
+                ivDogUpload.setImageURI(mPhotoSelectedUri)
+                btnImage.visibility = View.INVISIBLE
+            }
+
+        }
+
+
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         _binding = FragmentAddBinding.inflate(layoutInflater)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            btnImage.setOnClickListener {
+                openGallery()
+            }
+
+
+
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryLauncher.launch(intent)
+
     }
 
 
