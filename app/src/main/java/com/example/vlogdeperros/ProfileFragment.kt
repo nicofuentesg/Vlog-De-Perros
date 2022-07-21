0ec780d232e635d61fccd577cd191dba75e1c8cf
Nicolas <nicolasfuentesg06@gmail.com>
2022-07-21 15:15:02 -0300

    Realizamos el codigo para poder salir de la sesion actual del usuario

--- app/src/main/java/com/example/vlogdeperros/ProfileFragment.kt
+++ app/src/main/java/com/example/vlogdeperros/ProfileFragment.kt
@@ -5,55 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.vlogdeperros.databinding.ActivityMainBinding
+import com.example.vlogdeperros.databinding.FragmentProfileBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(layoutInflater)
+
+        takeValues()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun takeValues() {
+        binding.apply {
+            tvNameUser.text = FirebaseAuth.getInstance().currentUser?.displayName
+            tvMailUser.text = FirebaseAuth.getInstance().currentUser?.email
+            btnLogout.setOnClickListener {
+                signOut()
             }
+        }
     }
+
+    private fun signOut() {
+        context?.let {
+            AuthUI.getInstance().signOut(it).addOnSuccessListener {
+                Toast.makeText(context,getString(R.string.goodbye),Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
