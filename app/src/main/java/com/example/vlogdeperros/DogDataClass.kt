2dc128e9551417575e9713540a49a6781fb9069c
Nicolas <nicolasfuentesg06@gmail.com>
2022-07-21 16:06:42 -0300

    Agregamos el botton de me gusta y configuaramos el homeFragment para que se pueda eliminar la publicaciones vistas

--- app/src/main/java/com/example/vlogdeperros/DogDataClass.kt
+++ app/src/main/java/com/example/vlogdeperros/DogDataClass.kt
@@ -1,10 +1,11 @@
 package com.example.vlogdeperros
 
+import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
 
 
 @IgnoreExtraProperties
-data class DogDataClass(var id:String = "",
+data class DogDataClass(@Exclude var id:String = "",
                         var title:String = "",
                         var photoUrl:String = "",
                         var likeList: Map<String,Boolean> = mutableMapOf())
